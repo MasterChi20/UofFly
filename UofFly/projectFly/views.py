9c2c218d32aedd981533156d5ae440163731235d
Chirag Jain <masterchi20@icloud.com>
2019-02-23 09:52:34 -0600

    Updated django project structure. Please make sure all python code lives in /projectFly. All UI code will live in projectFly/templates/projectFly. Have not figured out how to display the html using views yet.

---
+++ UofFly/projectFly/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    context = {}
+    return render(request, 'projectFly/index.html', context)
+
