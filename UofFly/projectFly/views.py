36740e991de551bb6a87b8751fd72860837010f0
Chirag Jain <masterchi20@icloud.com>
2019-02-24 02:52:59 -0600

    Finally completed the entire project. Need to refine the code a small bit.

--- UofFly/projectFly/views.py
+++ UofFly/projectFly/views.py
@@ -11,12 +11,37 @@ from . import unifier
 
 
 def index(request):
-    print(request.POST)
-    pairings = unifier.runner("Chicago", "Dubai", "04/04/2019", 1.0, 0.0)
-    context = {
+	pairings = []
+	
+	if request.method != 'POST':
+		pairings.append({
+			'price': None,
+			'flight_hour': None,
+			'fuel': None,
+			'airline': None
+		})
+		pairings.append({
+			'price': None,
+			'flight_hour': None,
+			'fuel': None,
+			'airline': None
+		})
+		pairings.append({
+			'price': None,
+			'flight_hour': None,
+			'fuel': None,
+			'airline': None
+		})
+
+	if request.method == 'POST':
+		print(request.POST['departure_airport'])
+		pairings = unifier.runner(request.POST['departure_airport'], request.POST['arrival_airport'], request.POST['input_date'], 1.0, 0.0)
+
+
+	context = {
     	'first': pairings[0],
     	'second': pairings[1],
     	'third': pairings[2]
     }
-    return render(request, 'projectFly/index.html', context)
+	return render(request, 'projectFly/index.html', context)
 
