253c7160d71993605ec05579bab16de1d9d577a8
Chirag Jain <masterchi20@icloud.com>
2019-02-23 18:04:49 -0600

    Completed functionality to obtain fuel for trip.

--- UofFly/projectFly/trip.py
+++ UofFly/projectFly/trip.py
@@ -1,4 +1,5 @@
 import json
+import requests
 
 class Leg:
 	def __init__(self, mode, org, dest, time, duration, price, flight):
@@ -9,22 +10,44 @@ class Leg:
 		self.duration = duration
 		self.price = price
 		self.flight = flight
-		self.fuel = 0
+		self.fuel = 0.0
+
+	def getFuel(self):
+		URL = "http://www.distance24.org/route.json?stops="
+		URL = URL + self.org + "|" + self.dest
+		r = requests.get(url = "https://www.distance24.org/route.json?stops=Hamburg|Berlin")
+		data = r.json()
+		file = open("assets/FlightFuelData.json", "r")
+		flightData = json.loads(file.read())
+		self.fuel = int(data["distance"])
+		for x in range(0,len(flightData)):
+			if flightData[x]["model"] == self.flight:
+				a = float(data["distance"])
+				b = float(flightData[x]["fuel"])
+				self.fuel = a * b
+				break
+		return self.fuel
+
+
     
 class Trip:
 	def __init__(self):
 		self.legs = []
+		self.rank = 0
+		self.totalFuel = 0
 
 	def addLeg(self, mode, org, dest, time, duration, price, flight):
 		currentLeg = Leg(mode, org, dest, time, duration, price, flight)
+		currentLeg.getFuel()
+		self.totalFuel = self.totalFuel + currentLeg.fuel
 		self.legs.append(currentLeg)
 
-	def createFuel(self):
-		file = open("assets/fuelEfficiency.json", "r")
-		fileJson = json.loads(file.read())
-		print(fileJson[0]["fuelBurn"])
+
+
+		
 
 if __name__ == '__main__':
 	trip = Trip()
-	trip.addLeg(trip, "flight", "ord", "blr", 20, 1000, "A330")
-	trip.createFuel()
+	trip.addLeg("flight", "Chicago", "Berlin", "12:00", 20, 1000, "Airbus A330-200")
+	print(trip.totalFuel)
+
